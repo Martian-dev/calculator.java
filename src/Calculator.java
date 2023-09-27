14ae010779239afcd2d102e0a168e6a2ea18058d
Vaibhav <52103891+Martian-dev@users.noreply.github.com>
2023-09-27 11:04:52 +0530

    Added square, power, sqrt, sin, cos, tan functions and pi button

--- src/Calculator.java
+++ src/Calculator.java
@@ -9,30 +9,42 @@ public class Calculator implements ActionListener {
     static JPanel panel;
 
     static JButton[] numberButtons = new JButton[10];
-    static JButton[] functionButtons = new JButton[7];
-    static JButton addButton, subButton, divButton, multiButton, equButton, clrButton, delButton;
+    static JButton[] functionButtons = new JButton[13];
+    static JButton addButton, subButton, divButton, multiButton, sqButton, sqrtButton, powButton, equButton, clrButton, delButton, sinButton, cosButton, tanButton;
     static JButton button1, button2, button3, button4, button5, button6, button7, button8, button9, button0;
+    static JButton piButton;
 
     static char operation;
     static double num1;
     static double num2;
+    static double sngl;
 
     public static void main(String[] args) {
         frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(420, 550);
+        frame.setSize(520, 620);
+        frame.getContentPane().setBackground(Color.DARK_GRAY);
+        frame.setTitle("Calculator Da");
 
         txf = new JTextField();
-        txf.setBounds(50, 25, 300, 50);
+        txf.setBounds(50, 25, 400, 50);
         txf.setEditable(false);
+        txf.setBackground(Color.gray);
+        txf.setForeground(Color.white);
 
         addButton = new JButton("+");
         subButton = new JButton("-");
         multiButton = new JButton("x");
         divButton = new JButton("/");
+        sqButton = new JButton("x^2");
+        sqrtButton = new JButton("√");
+        powButton = new JButton("^");
         equButton = new JButton("=");
         clrButton = new JButton("AC");
         delButton = new JButton("del");
+        sinButton = new JButton("sin");
+        cosButton = new JButton("cos");
+        tanButton = new JButton("tan");
 
         functionButtons[0] = addButton;
         addButton.addActionListener(new Calculator());
@@ -42,12 +54,24 @@ public class Calculator implements ActionListener {
         multiButton.addActionListener(new Calculator());
         functionButtons[3] = divButton;
         divButton.addActionListener(new Calculator());
+        functionButtons[5] = sqButton;
+        sqButton.addActionListener(new Calculator());
+        functionButtons[6] = sqrtButton;
+        sqrtButton.addActionListener(new Calculator());
+        functionButtons[7] = powButton;
+        powButton.addActionListener(new Calculator());
         functionButtons[4] = equButton;
         equButton.addActionListener(new Calculator());
-        functionButtons[5] = clrButton;
+        functionButtons[8] = clrButton;
         clrButton.addActionListener(new Calculator());
-        functionButtons[6] = delButton;
+        functionButtons[9] = delButton;
         delButton.addActionListener(new Calculator());
+        functionButtons[10] = sinButton;
+        sinButton.addActionListener(new Calculator());
+        functionButtons[11] = cosButton;
+        cosButton.addActionListener(new Calculator());
+        functionButtons[12] = tanButton;
+        tanButton.addActionListener(new Calculator());
 
 
         button1 = new JButton("1");
@@ -80,6 +104,9 @@ public class Calculator implements ActionListener {
         button0 = new JButton("0");
         button0.addActionListener(new Calculator());
 
+        piButton = new JButton("π");
+        piButton.addActionListener(new Calculator());
+
         numberButtons[0] = button0;
         numberButtons[1] = button1;
         numberButtons[2] = button2;
@@ -91,37 +118,45 @@ public class Calculator implements ActionListener {
         numberButtons[8] = button8;
         numberButtons[9] = button9;
 
-        delButton.setBounds(50, 430, 145, 50);
-        clrButton.setBounds(205, 430, 145, 50);
-
+        delButton.setBounds(50, 470, 153, 50);
+        clrButton.setBounds(213, 470, 153, 50);
+        sqButton.setBounds(50, 410, 153, 50);
+        sqrtButton.setBounds(213, 410, 153, 50);
         panel = new JPanel();
-        panel.setBounds(50,100,300,300);
-        panel.setLayout(new GridLayout(4, 4, 10,10));
+        panel.setBounds(50,100,400,300);
+        panel.setLayout(new GridLayout(4, 5, 10,10));
+        panel.setBackground(Color.DARK_GRAY);
 
         for (int i = 1; i<=3; i++) {
             panel.add(numberButtons[i]);
         }
 
         panel.add(addButton);
+        panel.add(sinButton);
 
         for (int i = 4; i<=6; i++) {
             panel.add(numberButtons[i]);
         }
 
         panel.add(subButton);
+        panel.add(cosButton);
 
         for (int i = 7; i<=9; i++) {
             panel.add(numberButtons[i]);
         }
 
         panel.add(multiButton);
-        panel.add(new JButton());
+        panel.add(tanButton);
+        panel.add(powButton);
         panel.add(numberButtons[0]);
         panel.add(equButton);
         panel.add(divButton);
+        panel.add(piButton);
 
         frame.add(delButton);
         frame.add(clrButton);
+        frame.add(sqButton);
+        frame.add(sqrtButton);
         frame.add(txf);
         frame.add(panel);
 
@@ -153,6 +188,31 @@ public class Calculator implements ActionListener {
             txf.setText("");
             operation = '/';
         }
+        else if (e.getSource() == powButton) {
+            num1 = Double.parseDouble(txf.getText());
+            txf.setText("");
+            operation = '^';
+        }
+        else if (e.getSource() == sqButton) {
+            sngl = Double.parseDouble(txf.getText());
+            txf.setText(Double.toString(sngl * sngl));
+        }
+        else if (e.getSource() == sqrtButton) {
+            sngl = Double.parseDouble(txf.getText());
+            txf.setText(Double.toString(Math.sqrt(sngl)));
+        }
+        else if (e.getSource() == sinButton) {
+            sngl = Double.parseDouble(txf.getText());
+            txf.setText(Double.toString(Math.sin(Math.toRadians(sngl))));
+        }
+        else if (e.getSource() == cosButton) {
+            sngl = Double.parseDouble(txf.getText());
+            txf.setText(Double.toString(Math.cos(Math.toRadians(sngl))));
+        }
+        else if (e.getSource() == tanButton) {
+            sngl = Double.parseDouble(txf.getText());
+            txf.setText(Double.toString(Math.tan(Math.toRadians(sngl))));
+        }
         else if (e.getSource() == clrButton) {
             txf.setText("");
         }
@@ -162,6 +222,15 @@ public class Calculator implements ActionListener {
             for (int i =0; i<currentText.length()-1; i++) {
                 String currentInnerText = txf.getText();
                 txf.setText(currentInnerText + currentText.charAt(i));
+            }
+        }
+        else if (e.getSource() == piButton) {
+            if (!txf.getText().isEmpty()) {
+                num1 = Double.parseDouble(txf.getText());
+                txf.setText(Double.toString(num1*Math.PI));
+            }
+            else {
+                txf.setText(Double.toString(Math.PI));
             }
         }
         else if (e.getSource() == equButton) {
@@ -180,7 +249,15 @@ public class Calculator implements ActionListener {
                     break;
 
                 case '/':
+                    if (num2 == 0) {
+                        txf.setText("Cannot Divide By Zero");
+                        break;
+                    }
                     txf.setText(Double.toString(num1/num2));
+                    break;
+
+                case '^':
+                    txf.setText(Double.toString(Math.pow(num1, num2)));
                     break;
 
                 default:
